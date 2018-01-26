0dd58f9392d09c0a19ec60d177560ca22a9960d6
fun-cursus10 <fun-cursus10@pc150217.knmi.nl>
2018-01-26 11:04:12 +0000

    commiting!

--- R/area.R
+++ R/area.R
@@ -1,3 +1,8 @@
+#'Calculate area of square
+#'
+#'@param length The length of the square
+#'@param width The width of the square
+#'@export
 area_of_square<-function(length, width){
   area<-length*width
   return(area)
